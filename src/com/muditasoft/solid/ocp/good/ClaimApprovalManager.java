4930cc33ec6e109fbe727ea82268abab01e5675f
tutkuince <tutku.ince@outlook.com>
2018-11-21 15:26:43 +0300

    ClaimApprovalManager

---
+++ src/com/muditasoft/solid/ocp/good/ClaimApprovalManager.java
@@ -0,0 +1,9 @@
+package com.muditasoft.solid.ocp.good;
+
+public class ClaimApprovalManager {
+    public void processClaim(InsuranceSurveyor surveyor) {
+        if (surveyor.isValidClaim()) {
+            System.out.println("ClaimApprovalManager: Valid claim. Currently processing claim for approval....");
+        }
+    }
+}
