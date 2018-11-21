0ead1524b15aaaad3010f99378aad52641690a6f
tutkuince <tutku.ince@outlook.com>
2018-11-21 15:37:26 +0300

    TrasportationDevice

---
+++ src/com/muditasoft/solid/lsp/good/TrasportationDevice.java
@@ -0,0 +1,23 @@
+package com.muditasoft.solid.lsp.good;
+
+public class TrasportationDevice {
+    String name;
+
+    double speed;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+}
