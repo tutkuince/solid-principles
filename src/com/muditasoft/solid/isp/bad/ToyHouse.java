4fdc898c4f0d7ed7f69e7d15ef3a0fc7ed946e28
tutkuince <tutku.ince@outlook.com>
2018-11-21 15:45:19 +0300

    ToyHouse implements Toy

---
+++ src/com/muditasoft/solid/isp/bad/ToyHouse.java
@@ -0,0 +1,26 @@
+package com.muditasoft.solid.isp.bad;
+
+public class ToyHouse implements Toy {
+    private double price;
+    private String color;
+
+    @Override
+    public void setPrice(double price) {
+
+    }
+
+    @Override
+    public void setColor(String color) {
+
+    }
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
