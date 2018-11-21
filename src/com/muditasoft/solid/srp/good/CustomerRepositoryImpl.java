7355d700154d598c0e6ec26db8ff4befc1e6fc64
tutkuince <tutku.ince@outlook.com>
2018-11-21 15:13:14 +0300

    CustomerRepositoryImpl implements CustomerRepository

---
+++ src/com/muditasoft/solid/srp/good/CustomerRepositoryImpl.java
@@ -0,0 +1,18 @@
+package com.muditasoft.solid.srp.good;
+
+public class CustomerRepositoryImpl implements CustomerRepository {
+    @Override
+    public void create(Customer customer) {
+
+    }
+
+    @Override
+    public void update(Customer customer) {
+
+    }
+
+    @Override
+    public void delete(Customer customer) {
+
+    }
+}
