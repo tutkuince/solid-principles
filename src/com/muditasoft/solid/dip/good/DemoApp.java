5eafa06dfdd1481aae269df42f30bd7e217f61b3
tutkuince <tutku.ince@outlook.com>
2018-11-21 16:00:42 +0300

    DemoApp for Dependency Inversion Principle

---
+++ src/com/muditasoft/solid/dip/good/DemoApp.java
@@ -0,0 +1,15 @@
+package com.muditasoft.solid.dip.good;
+
+public class DemoApp {
+    public static void main(String[] args) {
+        Switchable switchableBulb = new LightBulb();
+        Switch bulbPowerSwitch = new ElectricPowerSwitch(switchableBulb);
+        bulbPowerSwitch.press();
+        bulbPowerSwitch.press();
+
+        Switchable switchableFan = new Fan();
+        Switch fanPowerSwitch = new ElectricPowerSwitch(switchableFan);
+        fanPowerSwitch.press();
+        fanPowerSwitch.press();
+    }
+}
