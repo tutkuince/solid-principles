a3c2a4c41ea485c38ac6bbbe0cfbc1d2e8d87443
tutkuince <tutku.ince@outlook.com>
2018-11-21 15:06:48 +0300

    Customer class with not appropriate sample

---
+++ src/com/muditasoft/solid/srp/bad/Customer.java
@@ -0,0 +1,34 @@
+package com.muditasoft.solid.srp.bad;
+
+import java.time.LocalDate;
+
+public class Customer {
+    private String name;
+    private String surname;
+    private LocalDate bithday;
+
+    public void create() {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void delete() {
+
+    }
+
+    public void sendEmail() {
+
+    }
+
+    public void login() {
+
+    }
+
+    public void createOrder() {
+
+    }
+
+}
