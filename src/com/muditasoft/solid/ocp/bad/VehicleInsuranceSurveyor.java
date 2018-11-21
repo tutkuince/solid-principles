ac38795aae8b45fd2a1a71d7311d132c02d8a274
tutkuince <tutku.ince@outlook.com>
2018-11-21 15:22:48 +0300

    VehicleInsuranceSurveyor

---
+++ src/com/muditasoft/solid/ocp/bad/VehicleInsuranceSurveyor.java
@@ -0,0 +1,9 @@
+package com.muditasoft.solid.ocp.bad;
+
+public class VehicleInsuranceSurveyor {
+    public boolean isValidClaim() {
+        System.out.println("VehicleInsuranceSurveyor: Validating health insurance claim...");
+        /*Logic to validate health insurance claims*/
+        return true;
+    }
+}
