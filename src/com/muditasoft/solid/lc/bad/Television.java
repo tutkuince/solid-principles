70fc92d016debbd355c9f18fb47fb4cacc9eedd4
tutkuince <tutku.ince@outlook.com>
2018-11-21 14:47:53 +0300

    RemoteControl

---
+++ src/com/muditasoft/solid/lc/bad/Television.java
@@ -0,0 +1,10 @@
+package com.muditasoft.solid.lc.bad;
+
+public class Television {
+    public void on(){
+        System.out.println("TV is opened.");
+    }
+    public void off(){
+        System.out.println("TV is closed");
+    }
+}
