a6f55bb6286022620e615c16098d7f9b9095da34
tutkuince <tutku.ince@outlook.com>
2018-11-21 15:40:55 +0300

    DevicesWithoutEngines extends TrasportationDevice

--- src/com/muditasoft/solid/lsp/good/DevicesWithoutEngines.java
+++ src/com/muditasoft/solid/lsp/good/DevicesWithoutEngines.java
@@ -1,6 +1,6 @@
 package com.muditasoft.solid.lsp.good;
 
-public class DevicesWithoutEngines {
+public class DevicesWithoutEngines extends TrasportationDevice {
     public void startMoving() {
         System.out.println("Started moving!");
     }
