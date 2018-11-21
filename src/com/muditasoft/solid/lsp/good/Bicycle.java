7bb68cdb4d9949f527efbd3dc8e31bbc5c34b8d3
tutkuince <tutku.ince@outlook.com>
2018-11-21 15:42:47 +0300

    Bicycle extends DevicesWithoutEngines

---
+++ src/com/muditasoft/solid/lsp/good/Bicycle.java
@@ -0,0 +1,8 @@
+package com.muditasoft.solid.lsp.good;
+
+public class Bicycle extends DevicesWithoutEngines {
+    @Override
+    public void startMoving() {
+        super.startMoving();
+    }
+}
